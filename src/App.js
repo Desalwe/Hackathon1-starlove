ebdcbfa4b8f16f4bc23ac7a16aaa69cd36982c8c
Alex Lawry-White <designalexweb@gmail.com>
2019-11-21 11:29:43 +0000

    applying data to the CharData state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import CharacterPackages from './CharacterPackages'
 import ChooseCharacter from './ChooseCharacter';
 import Preview from './Preview';
 import Top5 from './Top5';
@@ -11,58 +12,68 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      demo: 0
+      CharData: []
     }
+  }
 
+  componentDidMount() {
+    this.setState({ CharData: CharacterPackages });
+    console.log(this.state.CharData)
+  }
+
+  test = () => {
+    return console.log(this.state.CharData)
   }
 
 
   render() {
 
     return (
+      <>
+        <button onClick={this.test} >re-render</button>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Choose Character</Link>
+                </li>
+                <li>
+                  <Link to="/preview">Preview</Link>
+                </li>
+                <li>
+                  <Link to="/top5">Top 5</Link>
+                </li>
+                <li>
+                  <Link to="/chat">Chat</Link>
+                </li>
+              </ul>
+            </nav>
 
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Choose Character</Link>
-              </li>
-              <li>
-                <Link to="/preview">Preview</Link>
-              </li>
-              <li>
-                <Link to="/top5">Top 5</Link>
-              </li>
-              <li>
-                <Link to="/chat">Chat</Link>
-              </li>
-            </ul>
-          </nav>
-
-          {/* A <Switch> looks through its children <Route>s and
+            {/* A <Switch> looks through its children <Route>s and
               renders the first one that matches the current URL. */}
-          <Switch>
+            <Switch>
 
-            <Route exact path="/">
-              <ChooseCharacter />
-            </Route>
+              <Route exact path="/">
+                <ChooseCharacter />
+              </Route>
 
-            <Route exact path="/preview">
-              <Preview />
-            </Route>
+              <Route exact path="/preview">
+                <Preview />
+              </Route>
 
-            <Route exact path="/top5">
-              <Top5 />
-            </Route>
+              <Route exact path="/top5">
+                <Top5 />
+              </Route>
 
-            <Route exact path="/chat">
-              <Chat />
-            </Route>
+              <Route exact path="/chat">
+                <Chat />
+              </Route>
 
-          </Switch>
-        </div>
-      </Router>
+            </Switch>
+          </div>
+        </Router>
+      </>
     );
   }
 
