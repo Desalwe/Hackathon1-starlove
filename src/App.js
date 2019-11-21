2554faa0bb97d98d3c7ed747da069883ff98e2bc
Alex Lawry-White <designalexweb@gmail.com>
2019-11-21 10:49:36 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { Router, Switch, Route, Link } from "react-router-dom";
+import Chat from './Chat';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      demo: 0
+    }
+
+  }
+
+
+  render() {
+
+    return (
+
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              {/* <li>
+                <Link to="/">Choose Character</Link>
+              </li>
+              <li>
+                <Link to="/preview">Preview</Link>
+              </li>
+              <li>
+                <Link to="/top5">Top 5</Link>
+              </li> */}
+              <li>
+                <Link to="/chat">Chat</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            {/* <Route exact path="/">
+              <Choose-character />
+            </Route>
+
+            <Route exact path="/preview">
+              <Preview />
+            </Route>
+
+            <Route exact path="/top5">
+              <Top5 />
+            </Route> */}
+
+            <Route exact path="/chat">
+              <Chat />
+            </Route>
+
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+
+
+}
+
+export default App
